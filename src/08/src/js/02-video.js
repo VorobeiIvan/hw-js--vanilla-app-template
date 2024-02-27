25c9982ea6a61c892cd6da2d47f7588f96167f5c
VorobeiIvan <0675361905vi@gmail.com>
2024-02-27 21:29:50 +0200

    upd

--- src/08/src/js/02-video.js
+++ src/08/src/js/02-video.js
@@ -11,14 +11,14 @@
 //   allow="autoplay; encrypted-media"
 // ></iframe>
 
-//✅Виконуй це завдання у файлах 02-video.html і 02-video.js. Розбий його на декілька підзавдань:
-//✅Ознайомся з документацією бібліотеки Vimeo плеєра.
+//✅ Виконуй це завдання у файлах 02-video.html і 02-video.js. Розбий його на декілька підзавдань:
+//✅ Ознайомся з документацією бібліотеки Vimeo плеєра.
 //✅ Додай бібліотеку як залежність проекту через npm.
 //✅ Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player, але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
-// Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
-// Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time".
-// Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
-// Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
+//✅ Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
+//✅ Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time".
+//✅ Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
+//✅ Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
 
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
@@ -26,19 +26,21 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('play', sevedCurrentTime);
+player.on('play', savedCurrentTime);
 player.on('timeupdate', throttle(getCurrentTime, 1000));
 
-function sevedCurrentTime() {
-  const savedTime = localStorage.getItem('videoplayer-current-time');
+function savedCurrentTime() {
+  const savedTime = parseFloat(
+    localStorage.getItem('videoplayer-current-time')
+  );
   if (savedTime) {
     player
       .setCurrentTime(savedTime)
-      .then(currentTime => {
-        localStorage.setItem('videoplayer-current-time', currentTime);
+      .then(() => {
+        console.log('Playback time set to saved time:', savedTime);
       })
       .catch(error => {
-        console.error('Error getting current playback time:', error);
+        console.error('Error setting playback time:', error);
       });
   }
 }
