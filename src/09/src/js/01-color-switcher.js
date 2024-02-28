6e290072d7b8a10bbbfaacec01667518f71ee588
Ivan <0675361905vi@gmail.com>
2024-02-28 16:51:33 +0200

    upd

---
+++ src/09/src/js/01-color-switcher.js
@@ -0,0 +1,43 @@
+// Завдання 1 - перемикач кольорів
+// Виконуй це завдання у файлах 01-color-switcher.html і 01-color-switcher.js. Подивися демо-відео роботи перемикача.
+
+// HTML містить кнопки «Start» і «Stop».
+
+// <button type="button" data-start>Start</button>
+// <button type="button" data-stop>Stop</button>
+
+// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> на випадкове значення, використовуючи інлайн стиль. Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
+
+// УВАГА
+// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+// }
+
+const elements = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+elements.startBtn.addEventListener('click', onClickStartBtn);
+elements.stopBtn.addEventListener('click', onClickStopBtn);
+
+function onClickStartBtn() {
+  elements.startBtn.disabled = false;
+  elements.body.style.backgroundColor = getRandomHexColor();
+}
+function onClickStopBtn() {
+  elements.startBtn.disabled = true;
+  elements.startBtn.removeEventListener('click', onClickStartBtn);
+  elements.stopBtn.removeEventListener('click', onClickStopBtn);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
