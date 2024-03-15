096140d49d6bca85a51681d0e13afcd67585522e
Ivan <0675361905vi@gmail.com>
2024-03-15 17:17:35 +0200

    add start files for task 10

---
+++ src/10/src/cat-api.js
@@ -0,0 +1,16 @@
+// import axios from 'axios';
+
+// axios.defaults.headers.common["x-api-key"] = "live_buNPOUvrSFnbHmWMHbKKyTFK6Z5vmkgojVgP35PPtyfBbxDcEShhsDG9yaYGmrIK"
+
+// const BASE_URL = "https://api.thecatapi.com/v1";
+// const API_KEY = "live_buNPOUvrSFnbHmWMHbKKyTFK6Z5vmkgojVgP35PPtyfBbxDcEShhsDG9yaYGmrIK";
+// const END_POINT ="/breeds";
+// const SEARCH_URL = https://api.thecatapi.com/v1/images/search?breed_ids=ідентифікатор_породи
+
+
+// export function fetchBreeds() {}
+// export function  fetchCatByBreed(breedId){
+
+
+    
+// }
